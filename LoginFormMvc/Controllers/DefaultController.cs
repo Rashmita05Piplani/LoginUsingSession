a2eaf7f7ef635d3d54c73b6c0f086cd48c8640bd
Rashmita Piplani <rashmita.piplani@unthinkable.co>
2021-12-17 14:20:34 +0530

    Login and logout functionality

---
+++ LoginFormMvc/Controllers/DefaultController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoginFormMvc.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {   if (Session["username"] == null)
+                return RedirectToAction("Index", "Login");
+            return View();
+        }
+    }
+}
---
+++ LoginFormMvc/Controllers/LoginController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LoginFormMvc.Models;
+
+namespace LoginFormMvc.Controllers
+{
+    public class LoginController : Controller
+    {
+        LoginEntities db = new LoginEntities();
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(User user)
+        {   if (ModelState.IsValid == true)
+            {
+                var credentials = db.Users.Where(model => model.username == user.username && model.password == user.password).FirstOrDefault();
+                if (credentials == null)
+                {
+                    ViewBag.ErrorMessage = "Login Failed";
+                    return View();
+                }
+
+
+                else
+                {
+                    Session["username"] = user.username;
+                    return RedirectToAction("Index", "Default");
+                }
+            }
+            return View();
+            
+
+        }
+        public ActionResult Logout()
+        {
+            Session.Abandon();
+            return RedirectToAction("Index", "Login");
+        }
+        public ActionResult Reset()
+        {
+            ModelState.Clear();
+            return RedirectToAction("Index", "Login");
+            
+        }
+    }
+}
